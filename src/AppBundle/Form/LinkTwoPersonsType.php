43b82d5ab8c2ed5ea4c9a59f2f80a11699c6763d
Martin Rosser <martin.rosser@emchberger.ch>
2017-08-17 19:10:53 +0200

    Design improvements, simple role model

--- src/AppBundle/Form/LinkTwoPersonsType.php
+++ src/AppBundle/Form/LinkTwoPersonsType.php
@@ -2,16 +2,9 @@
 
 namespace AppBundle\Form;
 
-use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class LinkTwoPersonsType extends AbstractType
