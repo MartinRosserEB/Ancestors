29911dd17a7f500f6299c839d892480bed75fe98
Martin Rosser <martin.rosser@emchberger.ch>
2018-02-14 01:18:23 +0100

    Install Foundation scss / js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,17 @@
-var $ = require('jquery');
+import $ from 'jquery';
+import whatInput from 'what-input';
 
-require('../../node_modules/foundation-sites/scss/foundation.scss');
+window.$ = $;
+
+import Foundation from 'foundation-sites';
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+//import './lib/foundation-explicit-pieces';
+
+import dTree from 'd3-dtree';
+
+window.dTree = dTree;
+
+require('../scss/app.scss');
+
+$(document).foundation();
