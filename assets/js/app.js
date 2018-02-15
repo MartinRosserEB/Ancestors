57e8acab1e4fa21150f95a0106cc5a9787da0852
Martin Rosser <martin.rosser@emchberger.ch>
2018-02-15 22:48:29 +0100

    Add select2

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,13 +4,6 @@ import whatInput from 'what-input';
 window.$ = $;
 
 import Foundation from 'foundation-sites';
-// If you want to pick and choose which modules to include, comment out the above and uncomment
-// the line below
-//import './lib/foundation-explicit-pieces';
-
-$(document).ready(function() {
-    $(document).foundation();
-});
 
 var _ = require('lodash');
 import * as d3 from 'd3';
@@ -19,4 +12,18 @@ import dTree from 'd3-dtree';
 
 window.dTree = dTree;
 
+import 'select2';
+import 'select2/dist/css/select2.css';
+
 require('../scss/app.scss');
+
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+//import './lib/foundation-explicit-pieces';
+
+$(document).ready(function() {
+    $("select").select2();
+
+    $(document).foundation();
+});
+
