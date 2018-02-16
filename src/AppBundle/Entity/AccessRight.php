dc07ad198da94e274b15b29e3d29969d87cb504c
Martin Rosser <martin.rosser@emchberger.ch>
2018-02-16 21:41:56 +0100

    Add AccessRight entity

---
+++ src/AppBundle/Entity/AccessRight.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\AccessRightRepository")
+ */
+class AccessRight
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $accessType;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="FamilyTree")
+     */
+    private $familyTree;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User")
+     */
+    private $user;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setAccessType($type)
+    {
+        $this->accessType = $type;
+    }
+
+    public function getAccessType()
+    {
+        return $this->accessType;
+    }
+
+    public function setFamilyTree(FamilyTree $familyTree)
+    {
+        $this->familyTree = $familyTree;
+    }
+
+    public function getFamilyTree()
+    {
+        return $this->familyTree;
+    }
+
+    public function setUser(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
